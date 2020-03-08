ff579f290d9d654d99571c2f86649df06368207e
orkun mert <orkunmertgs41@gmail.com>
2020-03-08 13:45:30 +0300

    make it better

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -139,3 +139,6 @@ export function unregister() {
       });
   }
 }
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
